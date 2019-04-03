ce9854abf580bee0a6853fd27951ccfeda8a7257
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-03 21:11:14 +0400

    Add new class

---
+++ TodoList/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    public class Node<T>
+    {
+
+        public Node(T t)
+        {
+            Data = t;
+        }
+        public T Data { get; set; } 
+        public Node<T> Next { get; set; } 
+    }
+}
