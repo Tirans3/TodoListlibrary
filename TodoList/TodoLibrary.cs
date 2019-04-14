8cf0e84d70ec0ab3b84f09b2281df3114239ce1b
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-15 00:35:45 +0400

    Add methods All,Active,Complected

--- TodoList/TodoLibrary.cs
+++ TodoList/TodoLibrary.cs
@@ -11,18 +11,46 @@
                 int count = 0; int i = 0;
                 while (i != mylist.Count)
                 {
-                  count=mylist[i]._IsComplect==true ? count++ : ++i;
+                    count = mylist[i]._IsComplect? count++ : ++i;
                 }
                 return count;
             }
         }
 
-       
 
         public void Add(string str)
         {
             mylist.Add(new Task(str));
         }
 
+        public MyList<Task> All()
+        {
+            return mylist;
+        }
+
+        public MyList<Task> Active()
+        {
+            MyList < Task > temp= new MyList<Task>();
+            foreach(Task item in mylist)
+            {
+                if(!item._IsComplect)
+                {
+                    temp.Add(item);
+                }
+            }
+            return temp;
+        }
+        public MyList<Task> Complected()
+        {
+            MyList<Task> temp = new MyList<Task>();
+            foreach (Task item in mylist)
+            {
+                if (item._IsComplect)
+                {
+                    temp.Add(item);
+                }
+            }
+            return temp;
+        }
     }
 }
