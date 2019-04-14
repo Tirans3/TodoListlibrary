2f0d7c1948a78988cde20823599e63a9d4a84d57
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-15 00:16:49 +0400

    Update TodoLibrary

--- TodoList/TodoLibrary.cs
+++ TodoList/TodoLibrary.cs
@@ -1,89 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TodoList
+﻿namespace TodoList
 {
     class TodoLibrary
     {
-      public     MyList<string> mylist = new MyList<string>();
-        
-        private  void Welcome()
+        public MyList<Task> mylist = new MyList<Task>();
+
+        public int ItemLeft
         {
-            
-            Console.ResetColor();
-            Console.WriteLine(
-
-                              "To See all activ notes enter - F1\n\n" +
-
-                              "To Remove any notes enter    - F2\n\n" +
-
-                              "To Complet list enter        - F3\n");
-            
-                             
-            Console.WriteLine($"{mylist.Count}  Items\n");
-            ConsoleKeyInfo key = Console.ReadKey();
-
-            switch (key.Key)
+            get
             {
-                
-                case ConsoleKey.F1:
-                   Show();
-                    break;
-                case ConsoleKey.F2:
-                    Remove();
-                    break;
-                case ConsoleKey.F3:
-                   Complet();
-                    break;
-                default:
-                    Add();
-                    break;
-
+                int count = 0; int i = 0;
+                while (i != mylist.Count)
+                {
+                  count=mylist[i]._IsComplect==true ? count++ : ++i;
+                }
+                return count;
             }
-            
         }
 
-        public TodoLibrary()
+       
+
+        public void Add(string str)
         {
-            Welcome();
-
+            mylist.Add(new Task(str));
         }
-
-        void Add()
-        {
-            Console.Clear();
-            Console.WriteLine("Enter your  note\n");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            string s = Console.ReadLine();
-         
-            mylist.Add(s);
-            Welcome();
-
-        }
-
-        void Show()
-        {
-            
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            mylist.Show();
-            Welcome();
-        }
-        void Remove()
-        {
-            Console.WriteLine("Enter note number you wont remove");
-            int i = int.Parse(Console.ReadLine());
-            mylist.Remove(mylist[--i]);
-            Show();
-        }
-            void Complet()
-            {
-            Console.Clear();
-            Welcome();
-            }
 
     }
 }
